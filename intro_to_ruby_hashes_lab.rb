7a3115d31ee4bd68a575a6507e1309e7b2fa5ce4
Patrick Lynch <pajlynch@gmail.com>
2021-04-28 18:17:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,12 @@ def monopoly_with_third_tier
 		railroads: {
 			pieces: 4,
 			names: {},
-			rent_in_dollars: {}
+			rent_in_dollars: {
+				one_piece_owned:
+				two_pieces_owned:
+				three_pieces_owned:
+				four_pieces_owned:
+			}
 		}
 	}
 end
